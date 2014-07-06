d2a120ac3688a7434042018bdf3cab9bb95ff23b
Vasin Dmitriy <vasindima779@gmail.com>
2014-07-06 08:38:13 +0300

    Added simple js higliter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,11 @@
 //= require jquery_ujs
 //= require_self
 
-//= require highlighter/shCore
-//= require highlighter/shBrushJScript
-//= require highlighter/shBrushRuby
+//= require ./highlighter/shCore
+//= require ./highlighter/shBrushJScript
+//= require ./highlighter/shBrushRuby
+//= require ./highlighter/shBrushXml
+//= require ./highlighter/shBrushSass
 
 $(function() {
   SyntaxHighlighter.all();
