fcb3613b07e43ed112ca101214c1b0df698e3077
Vasin Dmitriy <vasindima779@gmail.com>
2014-09-22 22:01:39 +0300

    Refactoring off all files in posts dir

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,17 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.available_for(current_user).with_search(params[:search]).page(params[:page]).per(10)
+    @posts = Post.available_for(current_user).with_search(params[:search]).page(params[:page]).per(2)
     @posts = @posts.includes(:tags).references(:tags)
     find_tags
+    render_index
   end
 
   def show
     if find_post
       @relative_posts = Post.limit_rand(5)
       @tags = @post.tags
+      render_show
     else
       redirect_to root_path, alert: 'Post not find!' unless @post
     end
@@ -19,6 +21,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     find_tags
+    render_new
   end
 
   def create
@@ -27,14 +30,14 @@ class PostsController < ApplicationController
       redirect_to post_path(@post), notice: 'Post is successfully created!'
     else
       find_tags
-      render :new
+      render_new
     end
   end
 
   def edit
     find_post
     find_tags
-    render :new
+    render_new
   end
 
   def update
@@ -54,6 +57,7 @@ class PostsController < ApplicationController
   end
 
   private
+  # TODO: Need to refactor this part of private methods
   def find_post
     @post = Post.available_for(current_user).get_first(params[:id])
   end
@@ -68,5 +72,17 @@ class PostsController < ApplicationController
 
   def swich_params
     params.permit( :id, :state )
+  end
+
+  def render_new
+    render 'posts/new/new'
+  end
+
+  def render_show
+    render 'posts/show/show'
+  end
+
+  def render_index
+    render 'posts/index/index'
   end
 end
