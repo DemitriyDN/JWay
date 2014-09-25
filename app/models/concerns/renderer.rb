44c16e25825b9e1a54da930ae8303bbad2b18346
Vasin Dmitriy <vasindima779@gmail.com>
2014-09-26 00:37:44 +0300

    Escape text in Code fragment

--- app/models/concerns/renderer.rb
+++ app/models/concerns/renderer.rb
@@ -3,8 +3,15 @@ module Renderer
 
   private
   def render_body
-    extensions = {
-      quote: true,
+    renderer  = HTMLwithPygments.new()
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    text      = sweep_code_blocks(self.body)
+
+    self.rendered_body = redcarpet.render(text)
+  end
+
+  def extensions
+    {
       highlight: true,
       underline: true,
       strikethrough: true,
@@ -12,9 +19,15 @@ module Renderer
       fenced_code_blocks: true,
       no_intra_emphasis: true
     }
+  end
 
-    renderer = HTMLwithPygments.new()
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    self.rendered_body = redcarpet.render(self.body)
+  def sweep_code_blocks text
+    text.gsub( code_only ) do |unsafe_data|
+      "<code> #{ERB::Util.html_escape($1)} </code>"
+    end
+  end
+
+  def code_only
+    /<code>(.*?)<\/code>/
   end
 end
