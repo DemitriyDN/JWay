e423bdba815cad08ee28828bc34c3b9b64fd7df0
Vasin Dmitriy <vasindima779@gmail.com>
2014-10-04 22:06:10 +0300

    Refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,14 +5,12 @@ class PostsController < ApplicationController
     @posts = Post.available_for(current_user).with_search(params[:search]).page(params[:page]).per(10)
     @posts = @posts.includes(:tags).references(:tags)
     find_tags
-    render_index
   end
 
   def show
     if find_post
       @relative_posts = Post.limit_rand(5)
       @tags = @post.tags
-      render_show
     else
       redirect_to root_path, alert: 'Post not find!' unless @post
     end
@@ -21,7 +19,6 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     find_tags
-    render_new
   end
 
   def create
@@ -30,14 +27,14 @@ class PostsController < ApplicationController
       redirect_to post_path(@post), notice: 'Post is successfully created!'
     else
       find_tags
-      render_new
+      render :new
     end
   end
 
   def edit
     find_post
     find_tags
-    render_new
+    render :new
   end
 
   def update
@@ -72,17 +69,5 @@ class PostsController < ApplicationController
 
   def swich_params
     params.permit( :id, :state )
-  end
-
-  def render_new
-    render 'posts/new/new'
-  end
-
-  def render_show
-    render 'posts/show/show'
-  end
-
-  def render_index
-    render 'posts/index/index'
   end
 end
