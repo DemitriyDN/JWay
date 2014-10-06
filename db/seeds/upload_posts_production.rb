c6ee439c94e3bebb73d45959ee282ffec1bf1aa5
Vasin Dmitriy <vasindima779@gmail.com>
2014-10-06 23:09:22 +0300

    Changed seeds

--- db/seeds/upload_posts_production.rb
+++ db/seeds/upload_posts_production.rb
@@ -10,19 +10,6 @@ module UploadProduction
         Публиковаться будет код, материалы или статьи которые я нахожу интересными, или просто те, которые мне понравились и
         счел их нужным перевести или опубликовать.
       </p>
-      <p>
-        Не надо строго относится к статьям в этом блоге, я не претендую на абсолютные знания в какой бы
-        то ни было категории знаний ( даже в русском языке вы найдете много ошибок ;) ).
-      </p>
-      <p>
-      Если вы заметили ошибку либо заблуждение, вы всегда можете сообщить об этом в комментариях или просто
-        написать об этом на мой jway-rails@gmail.com
-      </p>
-      <p>
-        Также если вам понравилась какая-то статья и вы хотите поделиться ею с автором сайта - то пожалуйста
-            скидывайте ее по заданному емайлу:
-        <a href="mailto:j.way.rails@gmail.com">j.way.rails@gmail.com</a>
-      </p>
     '
     post.status = 1
     post.original = nil
@@ -38,47 +25,8 @@ module UploadProduction
       <p>
         Syntax Highliter
       </p>
-
-      <blockquote>
-        &nbsp;Post.rb
-      </blockquote>
-      <pre class="brush: ruby; first-line: 10; toolbar: false; highlight: [12, 14, 16]">
-      # Post.rb
-      class Post &lt; ActiveRecord::Base
-        has_and_belongs_to_many :tags
-
-        validates :title,
-                  :body,
-                  :body_title, presence: true
-        DISQUS_SHORTNAME = Rails.env == 'development' ? 'demitriydn'.freeze : 'j-way-rails'.freeze
-
-        enum status: {
-          verification: 0,
-          approved: 1
-        }
-
-        scope :by_date, -&gt; { order('created_at DESC') }
-        scope :available_for, -&gt; (user) { user ? by_date : where(status: 1).by_date }
-        scope :limit_rand, -&gt;(num) { limit(num).order('RANDOM()') }
-        scope :get_first, -&gt;(id) { where('id= ?', id.to_i).first }
-        scope :get_last_availible_id, -&gt; { where(status: 1).last.id }
-
-        def self.reject_blank_tags param
-          param[:tag_ids] = param[:tag_ids].reject(&:empty?)
-          param
-        end
-        def update_state state
-          if state == 'true'
-            self.approved!
-          else
-            self.verification!
-          end
-        end
-      end
-      </pre>
-
       <p>
-        Some new
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, commodi. Id atque commodi nisi cumque iste. Eum esse error fugiat doloribus odit. Sint libero dolores est laudantium, ratione deserunt dolorem.
       </p>
     TEXT
 
@@ -88,8 +36,7 @@ module UploadProduction
   end
 
   def add_tags
-    tags = ['HTML', 'CSS', 'Rake', 'Rack', 'Sublime', 'Ruby On Rails', 'Ruby', 'SQL',
-            'ActiveRecord', 'TDD', 'RSpec', 'JavaScript', 'Git', 'Gem', 'RVM', 'jQuery']
+    tags = ['HTML', 'Searching', 'Search']
 
     tags.each do |tag|
       Tag.create!({ name: tag })
